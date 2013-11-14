1e7195a36123f3fb0accc649f38a0c3037dc6f52
Rafał Muszyński <rmuszynski1@gmail.com>
2013-11-14 10:49:02 +0100

    fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('newscoop_topicmanagement_plugin');
+        $rootNode = $treeBuilder->root('newscoop_ticker_plugin');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
